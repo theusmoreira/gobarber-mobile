79c95e6af60aef11cfa01c3428a7362f8c4a405c
Matheus Santos <matheussm301@gmail.com>
2020-07-21 23:51:12 -0300

    chore: Add componentes de input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => {
-  return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-    </Container>
-  );
-}
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+    <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button
+      onPress={() => {
+        console.log('foi');
+      }}
+    >
+      Entrar
+    </Button>
+  </Container>
+);
 
 export default SignIn;
