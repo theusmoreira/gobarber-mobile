0e5da757dc8eacba574448a0c90589b71988fb55
Matheus Santos <matheussm301@gmail.com>
2020-07-23 05:37:18 -0300

    chore: Mudança de formatação

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {View, StatusBar} from 'react-native';
-import { NavigationContainer} from '@react-navigation/native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import Routes from './routes'
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return(
+  return (
     <NavigationContainer>
-      <StatusBar barStyle='light-content' backgroundColor='#312e38' />
-      <View style={{flex: 1, backgroundColor: '#312e38'}}>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
         <Routes />
       </View>
     </NavigationContainer>
-    );
+  );
 };
 
 export default App;
